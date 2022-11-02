447641a74cb9d58aa933cfc134182ba01886dfc1
Clement DAILLY <clement.dailly@epita.fr>
2022-11-02 21:27:12 +0100

    [ADD][USER] Add a new user

--- user.py
+++ user.py
@@ -1,8 +1,19 @@
 from PyInquirer import prompt
-user_questions = [
+import csv
 
+user_questions = [
+    {
+        "type":"input",
+        "name":"new_user",
+        "message":"New User : ",
+    }
 ]
 
 def add_user():
     # This function should create a new user, asking for its name
-    return
+    infos = prompt(user_questions)
+    with open('users.csv', 'a', newline='') as csvfile :
+        fieldnames = ['new_user']
+        writerfile = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writerfile.writerow(infos)
+    return True
